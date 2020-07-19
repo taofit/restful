d4f1c561dc65665f3aae667fe12a2d824f5bd1fd
Tao Sun <tsu@boozt.com>
2020-07-19 13:41:05 +0000

    just save all the files

---
+++ readme.md
@@ -0,0 +1,4 @@
+the project is to run rest api in php.
+Then connect to 127.0.0.1:8000 with Postman and send http requests. Note: when making PUT and POST requests,
+make sure to set the Body type to raw, then paste the payload in JSON format and set the content type to JSON (application/json).
+
---
+++ src/Controller/PersonController.php
@@ -0,0 +1,151 @@
+<?php
+namespace Src\Controller;
+use Src\TableGateways\PersonGateway;
+/**
+ * Class PersonController
+ *
+ * @package \\${NAMESPACE}
+ */
+class PersonController
+{
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $personGateway;
+
+    /**
+     * PersonController constructor.
+     *
+     * @param $db
+     * @param $requestMethod
+     * @param $userId
+     */
+    public function __construct($db, $requestMethod, $userId)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+        $this->userId = $userId;
+
+        $this->personGateway = new PersonGateway($db);
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                if ($this->userId) {
+                    $response = $this->getUser($this->userId);
+                } else {
+                    $response = $this->getAllUsers();
+                }
+                break;
+            case 'POST':
+                $response = $this->createUserFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateUserFromRequest($this->userId);
+                break;
+            case 'DELETE':
+                $response = $this->deleteUser($this->userId);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getAllUsers()
+    {
+        $result = $this->personGateway->findAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+
+        return $response;
+    }
+
+    private function getUser($id)
+    {
+        $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $reponse['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+
+        return $response;
+    }
+
+    private function createUserFromRequest()
+    {
+        $input = (array) json_encode(file_get_contents('php://input'), true);
+        if (! $this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->insert($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+
+        return $response;
+    }
+
+    private function updateUserFromRequest($id)
+    {
+        $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $input = (array) json_decode(file_get_contents('php://input'), true);
+        if (! $this->validatePerson($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->personGateway->update($id, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = 'User with id: ' . $id . ' is removed from the system...';
+
+        return $response;
+    }
+
+    private function deleteUser($id)
+    {
+        $result = $this->personGateway->find($id);
+        if (! $result) {
+            return $this->notFoundResponse($id);
+        }
+        $this->personGateway->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = 'user '.$id.' is deleted';
+
+        return $response;
+    }
+
+    private function validatePerson($input)
+    {
+        if (! isset($input['firstname']) || ! isset($input['lastname'])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = 'no result from this request';
+        return $response;
+    }
+
+}
